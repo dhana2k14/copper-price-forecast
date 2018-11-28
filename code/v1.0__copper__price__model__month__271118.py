f97e7f26ccb2a8c1679a36c1436711b7561d6fce
Dhanasekaran <dhanasekaranp@lntecc.com>
2018-11-28 18:16:43 +0530

    draft-model-weekly

--- code/v1.0__copper__price__model__month__271118.py
+++ code/v1.0__copper__price__model__month__271118.py
@@ -3,12 +3,63 @@
 # * this source code tested on tfp3.6 env with anaconda 
 
 import pandas as pd
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
 
 # data preparation 
 # 1. Read Copper LME prices 
 
-main_df = pd.read_excel("../data/Copper_LME_MCK.xlsx", sheet_name = 'Copper_LME_Price_History', skiprows = 1)
+print("Current Working Directory is %s" % os.getcwd())
+main_df = pd.read_excel("./data/Copper_LME_MCK.xlsx", sheet_name = 'Copper_LME_Price_History', skiprows = 1)
 main_df.drop(['3-Month', '15-Month', '27-Month'], axis = 1, inplace = True)
-main_df.head()
+main_df.sort_values('Date', ascending = False, inplace = True)
+main_df['Date'] = main_df['Date'].apply(lambda x: x - pd.offsets.Week(weekday = 6))
+main_df = main_df.groupby('Date').mean()
+
+# train-test split
+
+train = main_df.iloc[0:600,:]
+test = main_df.iloc[600:,:]
+
+x_train, y_train = [],[]
+for i in range(400, len(train)):
+    x_train.append(main_df.iloc[i-400:i,0])
+    y_train.append(main_df.iloc[i,0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+# lstm network
+
+model = Sequential()
+model.add(LSTM(50, input_shape=(x_train.shape[1], x_train.shape[2]), return_sequences = True))
+model.add(LSTM(50))
+model.add(Dense(1))
+model.compile(loss = 'mean_squared_error', optimizer = 'adam')
+model.fit(x_train, y_train, epochs = 1, batch_size = 1, verbose = 2)
+
+# prediction
+inputs = main_df.iloc[len(main_df) - len(test) - 400:]
+
+X_test = []
+for i in range(400, inputs.shape[0]):
+    X_test.append(inputs.iloc[i-400:i,0])    
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+pred_price = model.predict(X_test)
+
+# Plot
+
+train = main_df.iloc[:600]
+test = main_df[600:]
+test['predictions'] = pred_price
+plt.plot(train['Spot'])
+plt.plot(test[['Spot', 'predictions']])
+
+train.to_csv("./output/train-data.csv", index = True)
+test.to_csv("./output/test-data.csv", index = True)
 
 
