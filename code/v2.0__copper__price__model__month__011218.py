a65e40f842fe8c4fd8b199a4a92cf04c4ec78fae
Dhanasekaran <dhanasekaranp@lntecc.com>
2018-12-03 17:47:22 +0530

    commit as on 3dec18 eod

--- code/v2.0__copper__price__model__month__011218.py
+++ code/v2.0__copper__price__model__month__011218.py
@@ -45,56 +45,81 @@ scrap_df.tail()
 # Crude oil prices
 # Convert to weekly data
 
-oil_df = pd.read_csv("./data/crude_oil_df.csv", parse_dates = ['Date'])
-temp_df = pd.date_range(start = '2012-01-01', end = '2018-11-31', freq = 'W')
+oil_df = pd.read_csv("./data/crude_oil_df.csv", parse_dates = ['Date'], dtype = {'Crude_Oil_Index':np.float16})
+temp_df = pd.DataFrame(pd.date_range(start = pd.to_datetime('2012-01-01'), end = pd.to_datetime('2018-11-30'), freq = 'W'), columns = ['Date'])
+temp_df = pd.merge(temp_df, oil_df, how = 'left', on = 'Date')
+temp_df.tail()
 
-# train-test split
+# Copper demand & supply
 
-main_data = main_df.values
-train = main_df.iloc[0:600,:]
-test = main_df.iloc[600:,:]
+demand_df = pd.read_csv("./data/cu_demand_df.csv", parse_dates = ['Date'], dtype = {'Production':np.float16, 'Consumption':np.float16})
+temp_df = pd.DataFrame(pd.date_range(start = pd.to_datetime('1998-01-01'), end = pd.to_datetime('2019-12-31'), freq = 'MS'), columns = ['Date'])
+temp_df = pd.merge(temp_df, demand_df, how = 'left', on = 'Date')
+temp_df.tail()
 
-# normalise data
-scalar = MinMaxScaler(feature_range = (-1, 1))
-scaled_data = scalar.fit_transform(main_data)
 
-x_train, y_train = [],[]
-for i in range(400, len(train)):
-    x_train.append(scaled_data[i-400:i,0])
-    y_train.append(scaled_data[i,0])
 
-x_train, y_train = np.array(x_train), np.array(y_train)
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
-# lstm network
 
-model = Sequential()
-model.add(LSTM(50, input_shape=(x_train.shape[1], x_train.shape[2]), return_sequences = True))
-model.add(LSTM(50))
-model.add(Dense(1))
-model.compile(loss = 'mean_squared_error', optimizer = 'adam')
-model.fit(x_train, y_train, epochs = 1, batch_size = 1, verbose = 2)
 
-# prediction
-inputs = main_df.iloc[len(main_df) - len(test) - 400:].values
-inputs = inputs.reshape(-1, 1)
-inputs = scalar.transform(inputs)
 
-X_test = []
-for i in range(400, inputs.shape[0]):
-    X_test.append(inputs[i-400:i,0])    
-X_test = np.array(X_test)
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-pred_price = model.predict(X_test)
-pred_price = scalar.inverse_transform(pred_price)
 
-# Plot
 
-train = main_df.iloc[:600]
-test = main_df[600:]
-test['predictions'] = pred_price
-plt.plot(train['Spot'])
-plt.plot(test[['Spot', 'predictions']])
+
+
+
+
+
+
+
+
+## train-test split
+#
+#main_data = main_df.values
+#train = main_df.iloc[0:600,:]
+#test = main_df.iloc[600:,:]
+#
+## normalise data
+#scalar = MinMaxScaler(feature_range = (-1, 1))
+#scaled_data = scalar.fit_transform(main_data)
+#
+#x_train, y_train = [],[]
+#for i in range(400, len(train)):
+#    x_train.append(scaled_data[i-400:i,0])
+#    y_train.append(scaled_data[i,0])
+#
+#x_train, y_train = np.array(x_train), np.array(y_train)
+#x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+#
+## lstm network
+#
+#model = Sequential()
+#model.add(LSTM(50, input_shape=(x_train.shape[1], x_train.shape[2]), return_sequences = True))
+#model.add(LSTM(50))
+#model.add(Dense(1))
+#model.compile(loss = 'mean_squared_error', optimizer = 'adam')
+#model.fit(x_train, y_train, epochs = 1, batch_size = 1, verbose = 2)
+#
+## prediction
+#inputs = main_df.iloc[len(main_df) - len(test) - 400:].values
+#inputs = inputs.reshape(-1, 1)
+#inputs = scalar.transform(inputs)
+#
+#X_test = []
+#for i in range(400, inputs.shape[0]):
+#    X_test.append(inputs[i-400:i,0])    
+#X_test = np.array(X_test)
+#X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+#pred_price = model.predict(X_test)
+#pred_price = scalar.inverse_transform(pred_price)
+#
+## Plot
+#
+#train = main_df.iloc[:600]
+#test = main_df[600:]
+#test['predictions'] = pred_price
+#plt.plot(train['Spot'])
+#plt.plot(test[['Spot', 'predictions']])
 
 print(main_df['Date'].tail())
 print(main_df['Date'].tail().apply(lambda x: x + pd.offsets.Week(2)))
